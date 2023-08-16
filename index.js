4ef5eb2eb6be1ad6dba74f9b1d4162e779a8867f
Bharavi Lakhote <bharavi15@gmail.com>
2023-08-16 17:12:42 +0530

    Added Counter

---
+++ index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    fetch('https://mc3xepweq4y6sfw4yswnq2izke0hyvtz.lambda-url.ap-south-1.on.aws/count/bharavi15')
+    .then(function(response) {
+        return response.json();
+    }).then(function(response){
+        document.getElementById('visitorCount').innerText = response.value
+    })
+}
