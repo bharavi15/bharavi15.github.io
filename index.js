71b30a2c051670100e7ce5661d1cab0ce1971389
Bharavi Lakhote <bharavi15@gmail.com>
2024-03-02 23:35:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// window.onload = function() {
-//     fetch('https://mc3xepweq4y6sfw4yswnq2izke0hyvtz.lambda-url.ap-south-1.on.aws/count/bharavi15')
-//     .then(function(response) {
-//         return response.json();
-//     }).then(function(response){
-//         document.getElementById('visitorCount').innerText = '#'+response.value
-//     })
-// }
+window.onload = function() {
+    fetch('https://mc3xepweq4y6sfw4yswnq2izke0hyvtz.lambda-url.ap-south-1.on.aws/count/bharavi15')
+    .then(function(response) {
+        return response.json();
+    }).then(function(response){
+        document.getElementById('visitorCount').innerText = '#'+response.value
+    })
+}
 
 const anchors = document.querySelectorAll("a");
 
